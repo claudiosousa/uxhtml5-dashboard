0b2a3f9fff158698240c8298c355f500c9bd50f7
claudio sousa <claudio.sousa@sungard.com>
2014-09-09 18:07:15 +0200

    files

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+var fs = require('fs-extra');
+
 module.exports = {
     run: function (options) {
 		var generator = options.generator;
 		generator.composeWith("html5:dep", { options: { 'install': 'https://services.sungard.com/git/scm/uxl/ux-kendodataviz-dashboard.git#master' } }, function () {
 		    console.log('dashboard-installed');
 		});
+		debugger;
+		fs.copy('files', options.directory, function (a, b, c) {
+		    debugger;
+		    console.log('files copied');
+		})
     }
 };
